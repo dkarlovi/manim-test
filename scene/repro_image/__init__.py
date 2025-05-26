cacc79b04e53a2c127394286896592500a410721
Dalibor Karlović <dalibor.karlovic@sigwin.hr>
2025-05-26 16:45:06 +0200

    reproducer for the manim bug

---
+++ scene/repro_image/__init__.py
@@ -0,0 +1,10 @@
+from os.path import dirname
+from manim import Scene, ImageMobject
+
+class ReproImage(Scene):
+    def construct(self):
+        foot_left = ImageMobject(dirname(__file__) + "/foot-left.png")
+        
+        self.add(foot_left)
+        self.wait(2)
+
