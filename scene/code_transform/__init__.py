86edaf2cdc054bf7d175797468af0467992af2b1
Dalibor Karlović <dalibor.karlovic@sigwin.hr>
2025-06-24 12:33:23 +0200

    CS

--- scene/code_transform/__init__.py
+++ scene/code_transform/__init__.py
@@ -2,25 +2,28 @@ from os.path import dirname
 
 from manim import *
 
+
 class CodeTransform(Scene):
     def construct(self):
         code = self._code("seq1.js")
         self.play(Write(code))
-    
+
         # TODO: read the files from a directory instead of hardcoding them
         files = ["seq2.js", "seq3.js", "seq4.js"]
         for file in files:
             new_code = self._code(file)
-            self.play(Transform(code, new_code, replace_mobject_with_target_in_scene=True))
+            self.play(
+                Transform(code, new_code, replace_mobject_with_target_in_scene=True)
+            )
             self.pause(2)
             code = new_code
         self.remove(code)
         self.wait(2)
-    
+
     @staticmethod
     def _code(name):
         return Code(
-            code_file=dirname(__file__)+f"/{name}",
+            code_file=dirname(__file__) + f"/{name}",
             language="javascript",
-            add_line_numbers=False
+            add_line_numbers=False,
         )
